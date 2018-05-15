5f65065947d8124e4ba4f7f22af8b7bdcef27372
harik <pharika31@gmail.com>
2018-05-14 18:00:58 -0700

    updating dojo survey with validation

--- flask_fundamentals/dojo_survey/server.py
+++ flask_fundamentals/dojo_survey/server.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, session, redirect
+import re
+
 app = Flask(__name__)
+app.secret_key ='ThisisaSecret'
+
 @app.route('/')
 def root_route():
 	return render_template('index.html')
 @app.route('/result', methods=['POST'])
 def result():
-	result = request.form
-	return render_template("result.html",result=result)
+	errors = False
+	if len(request.form['name']) < 1:
+		flash("Name cannot be blank!","nameError")
+		errors = True
+	
+	elif len(request.form['comment']) < 1:
+		flash("Comment cannot be blank!","commentError")
+		errors = True
+	elif len(request.form['comment']) < 1:
+		flash("Comment cannot be more than 120 characters!","commentError")
+		errors = True
+	
+	if errors == True:
+		return redirect('/')
+	else:
+		result = [['Name', request.form['name']], ['Location', request.form['loc']], ['Language',request.form['lang']], ['Comment', request.form['comment']]]
+		return render_template("result.html",result=result)
 app.run(debug=True)
