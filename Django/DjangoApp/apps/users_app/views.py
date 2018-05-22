b5691d3c77a6c1ac8970fa1434388218c3e76477
harik <pharika31@gmail.com>
2018-05-22 13:12:36 -0700

    adding users_app to DjangoApp

---
+++ Django/DjangoApp/apps/users_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse, redirect
+  # the index function is called when root is visited
+def register(request):
+    response = "placeholder for users to create a new user record"
+    return HttpResponse(response)
+
+def index(request):
+    response = "placeholder to later display all the list of users"
+    return HttpResponse(response)
+def login(request):
+    response = "placeholder for users to login!"
+    return HttpResponse(response)
